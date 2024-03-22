d72aa6daa1fcfc79c733b48a7da716bfc0478408
anamariaow <new_annemarie@yahoo.com>
2024-03-22 01:46:08 +0100

    h2 mock

---
+++ src/main/java/com/example/h2mock/H2MockApplication.java
@@ -0,0 +1,14 @@
+package com.example.h2mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2MockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2MockApplication.class, args);
+	}
+
+}
+// http://localhost:8080/h2-console
---
+++ src/main/java/com/example/h2mock/entity/User.java
@@ -0,0 +1,51 @@
+package com.example.h2mock.entity;
+
+import jakarta.persistence.*;
+
+import javax.annotation.processing.Generated;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+
+    public User(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
